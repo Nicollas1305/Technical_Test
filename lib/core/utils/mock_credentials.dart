4a8e4e8d2b35585493d5f7803dfb378c0116cb6d
Nicollas1305 <cartola1320@gmail.com>
2024-11-22 19:30:43 -0300

    feat: add user login validation and credentials.

---
+++ lib/core/utils/mock_credentials.dart
@@ -0,0 +1,4 @@
+class MockCredentials {
+  static const String email = 'admin@admin.com';
+  static const String password = 'admin12345';
+}
--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:technical_test_mobile/core/utils/routes.dart';
+import '../../core/utils/mock_credentials.dart';
+import '../widgets/invalid_credentials_dialog.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,7 +13,20 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   bool showPassword = false;
+
+  void validateCredentials(BuildContext context) {
+    if (emailController.text == MockCredentials.email &&
+        passwordController.text == MockCredentials.password) {
+      Navigator.pushNamed(context, Routes.home);
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => const InvalidCredentialsDialog(),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,71 +36,92 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                contentPadding: EdgeInsets.all(20.0),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                labelText: 'Email',
-                labelStyle: TextStyle(
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(20.0),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14.0),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email não pode ser vazio.';
+                  } else if (!value.contains('@')) {
+                    return 'Email inválido.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                controller: passwordController,
+                obscureText: !showPassword,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  labelText: 'Senha',
+                  labelStyle: const TextStyle(
                     color: Colors.grey,
                     fontWeight: FontWeight.w600,
-                    fontSize: 14.0),
+                    fontSize: 14.0,
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        showPassword ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    },
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Senha não pode ser vazia.';
+                  } else if (value.length < 6) {
+                    return 'Senha deve conter pelo menos 6 caracteres.';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 20.0),
-            TextField(
-              controller: passwordController,
-              obscureText: !showPassword,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.all(20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
+              const SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState?.validate() ?? false) {
+                    validateCredentials(context);
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  minimumSize: const Size(double.infinity, 60.0),
                 ),
-                labelText: 'Senha',
-                labelStyle: const TextStyle(
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 14.0,
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                      showPassword ? Icons.visibility : Icons.visibility_off),
-                  onPressed: () {
-                    setState(() {
-                      showPassword = !showPassword;
-                    });
-                  },
-                ),
-              ),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, Routes.home);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).primaryColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                minimumSize: const Size(double.infinity, 60.0),
-              ),
-              child: const Text(
-                'Sign In',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 15.0,
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15.0,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
