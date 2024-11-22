9692a4a3f1b4f8c1f0907c1d66d195bba3cfdd98
Nicollas1305 <cartola1320@gmail.com>
2024-11-22 19:15:31 -0300

    feat: Implements home page with success messages.

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -10,6 +10,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home page"),
+      ),
+      body: const Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Usuário logado com sucesso!"),
+            SizedBox(height: 10.0),
+            Text("Bem-vindo ao app!"),
+          ],
+        ),
+      ),
+    );
   }
 }
