aa61890aa679ae9601fdfb61ba0b3e42ce7d2406
Nicollas1305 <cartola1320@gmail.com>
2024-11-22 19:39:41 -0300

    style: adds styling with similar color to those used by NicePlanet.

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Teste Mobile Pleno',
       initialRoute: Routes.login,
       onGenerateRoute: Routes.generateRoute,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF075999),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            minimumSize: const Size(double.infinity, 60.0),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              borderSide: BorderSide(color: Color(0xFF075999))),
+          labelStyle: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
     );
   }
 }
